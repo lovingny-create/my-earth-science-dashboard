eb2e21412ad2981c72dd681a007f5a2d5bc281bf
lovingny-create <loving.ny@gmail.com>
2026-03-11 14:03:38 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   const [currentPos, setCurrentPos] = useState({ dayIdx: -1, periodIdx: -1 });
   const [todoInput, setTodoInput] = useState("");
 
-  const NEIS_API_KEY = "여기에_받으신_인증키를_넣으세요"; 
+  const NEIS_API_KEY = "5bfe4967b9b64a3fb1693f1cc5371d50"; 
   const TIMETABLE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSfFMWDov9yz7QPfJOrdu15kgAdzhpJ-kMkn1zW6QjiRUzAecmNh4sPO9C3HzYmmlhl5xS5su3EWQSy/pub?gid=0&output=csv";
   const TODO_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSfFMWDov9yz7QPfJOrdu15kgAdzhpJ-kMkn1zW6QjiRUzAecmNh4sPO9C3HzYmmlhl5xS5su3EWQSy/pub?gid=210287103&output=csv";
   const WEATHER_API_KEY = "9addde09be74cdb63aab8481a0a207a0"; 
