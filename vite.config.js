a712f2a697e2c68d466eb89cbd9bc9fa33111daf
lovingny-create <loving.ny@gmail.com>
2026-03-11 11:09:18 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015' // 옛날 브라우저(ES6)도 지원하도록 설정
+  }
 })
